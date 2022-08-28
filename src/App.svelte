fb213afd728a2a3c0a47eb2b8c0c1552f3c19707
VexdStudios <36055611+Zaxiify@users.noreply.github.com>
2022-08-28 03:41:55 -0500

    gaming 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,37 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	import "fluent-svelte/theme.css";
+	import Buttons from './lib/Buttons.svelte';
+	import Test from './lib/Test.svelte';
+	import { TextBlock } from "fluent-svelte";
+	import { onMount } from 'svelte';
+	import { fade, scale } from 'svelte/transition';
+  
+	let ready = false;
+	onMount(() => ready = true);
+  </script>
+  
+  <main>
+	{#if ready}
+	<div>
+	  
+	</div>
+	  <div class="content">
+		<TextBlock ref="title" variant="display">rpa</TextBlock>
+	  </div>
+	  <TextBlock variant="subtitle">up n' coming lua dev</TextBlock>
+  
+	<div class="buttons">
+	  <p in:scale> </p>
+	  <hr in:scale />
+	  <p in:scale> </p>
+	  <Buttons />
+	</div>
+	{/if}
+  </main>
+  
+  <style>
+	:global([ref=title]) {
+	  font-family: monospace;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  </style>
+  
